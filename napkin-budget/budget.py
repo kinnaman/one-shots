fe0022c04d8a03e6ed7114d1156d2e0aab46ea1a
Morgan Kinnaman <dean.kinnaman@gmail.com>
2022-05-01 14:34:21 -0700

    napkin-budget init

---
+++ napkin-budget/budget.py
@@ -0,0 +1,51 @@
+# Program:  napkin-budget
+# Authour:  Morgan Kinnaman
+# License:  GPL-3.0
+# About:    This is a quick program written in Python3 for estimating future budgetting
+
+# Variables
+
+print('''
+_____ _  _ _ ____ _  _ 
+|  | |  | | |    |_/  
+|_\| |__| | |___ | \_ 
+__  _  _ ___  ____ ____ ___ 
+|__] |  | |  \ | __ |___  |  
+|__] |__| |__/ |__] |___  |
+
+Quickly calculate your budget with this app.
+''')
+
+while True:
+    print('''
+    Options
+    -----------
+    1. Expenses
+    2. Income
+    3. Investments
+    4. Graphs
+    5. Exit
+    ''')
+    option = input("Please select a category")
+    if option == "5":
+        break
+    if option == "1":
+        
+    if option == "2":
+        # income class
+    if option == "3":
+        # investment class
+    if option == "4":
+        # graphs class
+
+def expenses(itemized):
+    while True:
+        print('''
+        Would you like to:
+        1. Add an expense
+        2. Remove an expense
+        3. 
+        ''')
+        break
+    return itemized
+        
