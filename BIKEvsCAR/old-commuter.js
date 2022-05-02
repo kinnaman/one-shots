efea891bb790d8a9c2e1d7636120074f3fc53ab3
Morgan Kinnaman <dean.kinnaman@gmail.com>
2022-05-01 22:28:20 -0700

    added selector for values

--- BIKEvsCAR/old-commuter.js
+++ BIKEvsCAR/old-commuter.js
@@ -5,10 +5,10 @@
  */
 
 // element ids set as variables 
-var distance		= document.getElementById("distance");
-var carType 		= document.getElementById("carType");
-var mpg 			= document.getElementById("mpg");
-var gas			= document.getElementsByName("gasPrice"); // https://www.eia.gov/petroleum/gasdiesel/includes/gas_diesel_rss.xml
+var distanceId		= document.getElementById("distance");
+var carTypeId 		= document.getElementById("carType");
+var mpgId 			= document.getElementById("mpg");
+var gasId			= document.getElementsByName("gasPrice"); // https://www.eia.gov/petroleum/gasdiesel/includes/gas_diesel_rss.xml
 var insuranceId	= document.getElementById("insurance");
 var timeId			= document.getElementById("time");
 var output 			= document.getElementById("output");
@@ -17,8 +17,8 @@ function results() {
 	carTypeId 		= document.getElementById("carType");
 	//output.innerHTML = "some text"+ distanceId.value;
 	//console.log(car.mpg);
-	console.log(distance.value);
-	console.log(carType.value);
+	console.log(distanceId.value);
+	console.log(car.type);
 }
 
 // Tires, maintenance, repair, per mile according to AAA in cents
@@ -31,18 +31,25 @@ var wearPickup 		= 22.21;
 var wearHybrid 		= 12.58;
 var wearElectric 		= 10.23;
 
-var carWear = function(){
-	switch(carType){
-		case "small sedan": 	return wearSmallSedan;	
-		case "medium sedan": return wearMediumSedan; 	
-		case "large sedan": 	return wearLargeSedan;	
-		case "small suv": 	return wearSmallSUV;	
-		case "medium suv":	return wearMediumSUV; 	
-		case "pickup": 		return wearPickup;	
-		case "hybrid":			return wearHybrid;		
-		case "electric":     return wearElectric; 		
-	};
+var car = {
+	type: 		carTypeId.value,
+	//mpg: 			mpgId.value,
+	//insurance: 	insuranceId.value,
+	//time: 		timeId.value,
+	/*maintenance: function(){
+		switch(car.type){
+			case "small sedan": 	return wearSmallSedan;	
+			case "medium sedan": return wearMediumSedan; 	
+			case "large sedan": 	return wearLargeSedan;	
+			case "small suv": 	return wearSmallSUV;	
+			case "medium suv":	return wearMediumSUV; 	
+			case "pickup": 		return wearPickup;	
+			case "hybrid":			return wearHybrid;		
+			case "electric":     return wearElectric; 		
+		}
+	}*/
 };
+
 
 /*
 var bicycle = {
